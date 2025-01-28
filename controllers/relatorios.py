2c9f29e11ede206517a129f35a562ec45bf8b3a0
Hiandro Alex Silva de Medeiros <20221101110006@ifrn.local>
2025-01-28 17:34:18 -0300

    products not sold filter is working

--- controllers/relatorios.py
+++ controllers/relatorios.py
@@ -3,7 +3,7 @@ from models.vendas import Venda
 from models.clientes import Cliente
 from models.produtos import Produto
 from models.vendasprodutos import VendaProdutos
-from sqlalchemy import text
+from sqlalchemy import text, func, select
 from database.config import session
 
 relatorio_bp = Blueprint(name='relatorio', import_name=__name__, template_folder='templates', url_prefix='/relatorios')
@@ -12,6 +12,7 @@ relatorio_bp = Blueprint(name='relatorio', import_name=__name__, template_folder
 def filtros():
     consulta = []
     dias = request.form.get("dias", type=int, default=7)
+    result = []
     print("metodo post")
     # Obter o número de dias do formulário
 
@@ -40,8 +41,28 @@ WHERE pro_id NOT IN (
 """)
         consulta = session.execute(sql_sqlite, {"dias": dias}).fetchall()
         print(consulta)
+
+
+        # Subquery explícita
+        subquery = (
+            session.query(VendaProdutos.vpr_pro_id)
+            .join(Venda, VendaProdutos.vpr_ven_id == Venda.ven_id)
+            .filter(Venda.ven_data >= func.date('now', f'-{dias} days'))  # Usar função compatível com SQLite
+            .subquery()
+        )
+
+        # Query principal
+        query = (
+            session.query(Produto.pro_nome, Produto.pro_preco, Produto.pro_estoque)
+            .filter(Produto.pro_nome.not_in(select(subquery)))  # Força o uso de SELECT
+        )
+
+        # Executar a consulta
+        result = query.all()
+        print("Resultado da consulta:", result)
+
     except Exception as e:
         print(f"Erro ao gerar relatório: {str(e)}", "error")
         flash(f"Erro ao gerar relatório: {str(e)}", "error")
     finally:
-        return render_template("relatorios/filtros.html", produtos = consulta)
+        return render_template("relatorios/filtros.html", produtos=result)
