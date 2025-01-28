2de6b2794ea25644e4fa1335b0ece0e0a4a54b50
hiandro6 <hiandroalex6@gmail.com>
2025-01-28 09:59:00 -0300

    tentando fazer os relatorios

--- controllers/relatorios.py
+++ controllers/relatorios.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, url_for, request, render_template
+from flask import Blueprint, redirect, url_for, request, render_template, flash
 from models.vendas import Venda
 from models.clientes import Cliente
 from models.produtos import Produto
@@ -6,16 +6,41 @@ from models.vendasprodutos import VendaProdutos
 from sqlalchemy import text
 from database.config import session
 
-relatorios_bp = Blueprint(name='relatorio', import_name=__name__, template_folder='templates', url_prefix='/relatorios')
+relatorio_bp = Blueprint(name='relatorio', import_name=__name__, template_folder='templates', url_prefix='/relatorios')
 
-relatorios_bp.route('/', methods=['GET', 'POST'])
+@relatorio_bp.route('/', methods=['GET', 'POST'])
 def filtros():
-    if request.method == 'POST':
-        sql = text("""SELECT pro_nome FROM tb_produtos WHERE pro_id NOT IN (
-		SELECT vpr_pro_id FROM tb_vendas_produtos
-		JOIN tb_vendas ON vpr_ven_id = ven_id
-		WHERE ven_data >= CURDATE() - INTERVAL 7 DAY);""")
-        consulta = session.execute(sql)
-        pass
-    else:
-        return render_template("relatorios/filtros.html", produtos = consulta)
+    consulta = []
+    dias = request.form.get("dias", type=int, default=7)
+    print("metodo post")
+    # Obter o número de dias do formulário
+
+    try:
+        # Consultar os produtos que não foram vendidos nos últimos X dias
+        sql_mysql = text("""
+            SELECT pro_nome, pro_preco, pro_estoque 
+            FROM tb_produtos 
+            WHERE pro_id NOT IN (
+                SELECT vpr_pro_id 
+                FROM tb_vendas_produtos
+                JOIN tb_vendas ON vpr_ven_id = ven_id
+                WHERE ven_data >= CURDATE() - INTERVAL :dias DAY
+            )
+        """)
+
+        sql_sqlite = text("""
+SELECT pro_nome, pro_preco, pro_estoque 
+FROM tb_produtos 
+WHERE pro_id NOT IN (
+    SELECT DISTINCT vpr_pro_id 
+    FROM tb_vendas_produtos
+    JOIN tb_vendas ON vpr_ven_id = ven_id
+    WHERE ven_data >= DATE('now', '-? days')
+)
+""")
+        consulta = session.execute(sql_sqlite, (dias,)).fetchall()
+        print(consulta)
+    except Exception as e:
+        print(f"Erro ao gerar relatório: {str(e)}", "error")
+        flash(f"Erro ao gerar relatório: {str(e)}", "error")
+    return render_template("relatorios/filtros.html", produtos = consulta)
